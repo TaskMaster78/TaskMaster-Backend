4451d1a15ec372c25571ffabbb2dc345607bafd8
Hadi87s <hadiirshaid8722@gmail.com>
2025-05-15 13:55:38 +0300

    allowing the frontend to access the server.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import { schema } from './schema/schema';
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { schema } from "./schema/schema";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,14 +11,19 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI as string)
-  .then(() => console.log('✅ Connected to MongoDB Atlas'))
-  .catch(err => console.error('❌ MongoDB error:', err));
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => console.log("✅ Connected to MongoDB Atlas"))
+  .catch((err) => console.error("❌ MongoDB error:", err));
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}/graphql`);
